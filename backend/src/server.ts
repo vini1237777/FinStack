cb79faff6e5483d9a42d95ced1dc5e832c90c731
Vinisha Yadav <vinishadv@gmail.com>
2026-03-29 20:16:09 +0530

    Implemented and updated expenses route and services

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,6 +5,7 @@ import authRoutes from "./modules/auth/auth.routes";
 import mastersRoutes from "./modules/masters/masters.routes";
 import salesRoutes from "./modules/sales/sales.routes";
 import purchaseRoutes from "./modules/purchase/purchase.routes";
+import expenseRoutes from "./modules/expenses/expense.routes";
 
 dotenv.config();
 
@@ -22,6 +23,7 @@ app.use("/auth", authRoutes);
 app.use("/masters", mastersRoutes);
 app.use("/sales", salesRoutes);
 app.use("/purchase", purchaseRoutes);
+app.use("/expenses", expenseRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
