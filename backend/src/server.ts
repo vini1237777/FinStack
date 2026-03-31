c786bd5a9618d045436aba149666b53e3fcfd8ce
Vinisha Yadav <vinishadv@gmail.com>
2026-03-31 16:34:47 +0530

    minor change cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://your-vercel-url.vercel.app"],
+    origin: ["http://localhost:3000"],
   }),
 );
 app.use(express.json());
