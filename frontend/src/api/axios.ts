91e9f6376f44e775058a1f57059cbb74714d6cfb
Vinisha Yadav <vinishadv@gmail.com>
2026-03-31 15:53:06 +0530

    updated env in axios file

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5173",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
 });
-
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
   if (token) {
