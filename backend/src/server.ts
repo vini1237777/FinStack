18f1f23cc6596d311ddb73c55cd81f22fa8afa6b
Vinisha Yadav <vinishadv@gmail.com>
2026-03-31 16:36:04 +0530

    updated it

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,25 +1,23 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 import authRoutes from "./modules/auth/auth.routes";
 import mastersRoutes from "./modules/masters/masters.routes";
 import salesRoutes from "./modules/sales/sales.routes";
 import purchaseRoutes from "./modules/purchase/purchase.routes";
 import expenseRoutes from "./modules/expenses/expense.routes";
 import reportsRoutes from "./modules/reports/reports.routes";
-import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-  }),
-);
+app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(process.cwd(), "public")));
 
 app.use("/auth", authRoutes);
 app.use("/masters", mastersRoutes);
@@ -28,7 +26,6 @@ app.use("/purchase", purchaseRoutes);
 app.use("/expenses", expenseRoutes);
 app.use("/reports", reportsRoutes);
 
-app.use(express.static(path.join(process.cwd(), "public")));
 app.get("/{*path}", (req, res) => {
   res.sendFile(path.join(process.cwd(), "public", "index.html"));
 });
