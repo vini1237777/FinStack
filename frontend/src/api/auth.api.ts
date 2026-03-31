d8a59dd37ea3b1d9f45da71ed5521aaf460c7051
Vinisha Yadav <vinishadv@gmail.com>
2026-03-31 11:53:23 +0530

    changed post to login

--- frontend/src/api/auth.api.ts
+++ frontend/src/api/auth.api.ts
@@ -2,7 +2,7 @@ import api from "./axios";
 
 export const authApi = {
   login: (email: string, password: string) => {
-    return api.post("/auth/post", { email, password });
+    return api.post("/auth/login", { email, password });
   },
 
   register: (name: string, email: string, password: string) => {
